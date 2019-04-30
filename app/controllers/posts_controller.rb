879881f1ce2019faed07685e2ad920fa82e81880
katoharuki <kuromitsu3719@gmail.com>
2019-04-30 13:23:44 +0000

    formにまとめた。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,9 +12,26 @@ class PostsController < ApplicationController
         Post.create(post_params)
     end
 
-    def open
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def show
 #        binding.pry
       @post = Post.find(params[:id])
+    end
+
+    def update
+        post = Post.find(params[:id])
+        # モデルの更新は、クラスメソッドのupdateメソッドで行える
+        post.update(post_params)
+
+        # リダイレクト処理
+        redirect_to post
+    end
+
+    def open
+        @post = Post.find(params[:id])
     end
 
     private
