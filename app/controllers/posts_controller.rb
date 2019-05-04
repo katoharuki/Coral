0917d0a0c569b49fdc157180a6425e746d341744
katoharuki <kuromitsu3719@gmail.com>
2019-05-04 12:03:04 +0000

    バリデーションまではこのブランチ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,41 +1,71 @@
-# Applicationcontrollerクラスを継承することで、クラスがコントローラと認識される
 class PostsController < ApplicationController
-    def Coral
-        @posts = Post.all
+    before_action :set_target_post, only: %i[show edit update destroy]
+
+
+    def index
+        @posts = Post.page(params[:page])
     end
+
 
     def new
         @post = Post.new
     end
 
-   def create
-        Post.create(post_params)
-    end
 
-    def edit
-        @post = Post.find(params[:id])
-    end
+    def create
+        post = Post.create(post_params)
 
-    def show
-#        binding.pry
-      @post = Post.find(params[:id])
-    end
 
-    def update
-        post = Post.find(params[:id])
-        # モデルの更新は、クラスメソッドのupdateメソッドで行える
-        post.update(post_params)
+        # フラッシュ
+        flash[:notice] = "「#{post.title}」の記事が投稿されました!"
 
-        # リダイレクト処理
+
         redirect_to post
     end
 
-    def open
-        @post = Post.find(params[:id])
+
+    def show
     end
 
+
+    def edit
+    end
+
+
+    def update
+        @post.update(post_params)
+
+
+        # フラッシュ
+        flash[:notice] = "「#{@post.title}」の記事を更新しました!"
+
+
+        redirect_to @post
+    end
+
+
+    def destroy
+        @post.delete
+
+
+        # フラッシュ 
+        flash[:notice] = "「#{@post.title}」の記事を削除しました!"
+
+
+        redirect_to posts_path
+    end
+
+
     private
+
+
     def post_params
         params.require(:post).permit(:name, :title, :content)
     end
+
+
+    def set_target_post
+        @post = Post.find(params[:id])
+    end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  root 'posts#Coral'
-  get '', to: 'posts#Coral'
-  get 'posts', to: 'posts#Coral'
-
-  get 'posts/new', to: 'posts#new'
-
-  post 'posts', to: 'posts#create'
-
-  get 'posts/:id', to: 'posts#open'
-
-   resources :posts, only: [:Coral, :new, :create, :show, :edit, :update]
-
+    root 'posts#index'
+    # onlyを削除
+    resources :posts
 end
