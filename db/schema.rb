2a60d2d3bde5dd96aaf8aa52b9a831e6e8ec580d
katoharuki <kuromitsu3719@gmail.com>
2019-08-17 14:43:01 +0000

    model user def add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190728114321) do
+ActiveRecord::Schema.define(version: 20190813091433) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20190728114321) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "tags", force: :cascade do |t|
